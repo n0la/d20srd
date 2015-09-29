afb5035836aa6a41b50118d1e54294d61b5e0b68
Florian Stinglmayr <florian@n0la.org>
2015-09-29 21:16:33 +0200

    Added wondrous items.

--- lib/artefact_weapons.lua
+++
@@ -1,8 +0,0 @@
---- Function for generating special artefacts and their properties.
---- We must return a table containing all the functions that we wish
---- to export. We can also access the entire fimbul library.
----
-
-local lib = {}
-
-return lib
